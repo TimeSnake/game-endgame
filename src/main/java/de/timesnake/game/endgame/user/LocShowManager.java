cc73366e3d13d66e50ad740d1751a35e9cccd8bf
funzter <cavehive@protonmail.com>
2023-02-24 17:29:10 +0100

    refactored code

--- src/main/java/de/timesnake/game/endgame/user/LocShowManager.java
+++ src/main/java/de/timesnake/game/endgame/user/LocShowManager.java
@@ -7,16 +7,16 @@ package de.timesnake.game.endgame.user;
 import de.timesnake.basic.bukkit.util.Server;
 import de.timesnake.basic.bukkit.util.user.User;
 import de.timesnake.basic.bukkit.util.user.scoreboard.Sideboard;
+import de.timesnake.basic.bukkit.util.user.scoreboard.SideboardBuilder;
 import de.timesnake.basic.bukkit.util.world.ExLocation;
 import de.timesnake.game.endgame.main.GameEndGame;
 import de.timesnake.game.endgame.server.EndGameServer;
 import de.timesnake.library.basic.util.Tuple;
+import java.util.HashMap;
+import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
-
-import java.util.HashMap;
-import java.util.UUID;
 
 public class LocShowManager {
 
@@ -37,12 +37,15 @@ public class LocShowManager {
             this.trackedLocationByUser.get(user).getB().cancel();
         }
 
-        Sideboard sideboard = EndGameServer.getScoreboardManager().registerSideboard("endgameloc", "§6§lLocation");
-        sideboard.setScore(6, "§cName: §f" + name);
-        sideboard.setScore(5, "-------------------");
-        sideboard.setScore(4, "§9X: §f" + location.getBlockX());
-        sideboard.setScore(3, "§9Y: §f" + location.getBlockY());
-        sideboard.setScore(2, "§9Z: §f" + location.getBlockZ());
+        Sideboard sideboard = EndGameServer.getScoreboardManager()
+                .registerSideboard(new SideboardBuilder()
+                        .name("endgameloc")
+                        .title("§6§lLocation")
+                        .setScore(6, "§cName: §f" + name)
+                        .setScore(5, "-------------------")
+                        .setScore(4, "§9X: §f" + location.getBlockX())
+                        .setScore(3, "§9Y: §f" + location.getBlockY())
+                        .setScore(2, "§9Z: §f" + location.getBlockZ()));
         user.setSideboard(sideboard);
 
         BukkitTask task = Server.runTaskTimerSynchrony(() -> {
@@ -60,7 +63,8 @@ public class LocShowManager {
 
             if (locVector.clone().setY(0).length() > 4) {
                 double locAngle = Math.atan2(locVector.getZ(), locVector.getX());
-                double userAngle = Math.atan2(userLoc.getDirection().getZ(), userLoc.getDirection().getX());
+                double userAngle = Math.atan2(userLoc.getDirection().getZ(),
+                        userLoc.getDirection().getX());
 
                 double angle = userAngle - locAngle;
 
